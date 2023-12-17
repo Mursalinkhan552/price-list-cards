aea2e5ce6b5df623eeaea9d0d4dc2f3b9e04b147
unknown <mursalinkhan552@gmail.com>
2023-12-18 01:00:08 +0600

    BarChart And  LineChart Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import PriceList from './components/PriceList/PriceList'
+import Dashboard from './components/Dashboard/Dashboard'
+import PhoneBar from './components/PhoneBar/PhoneBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +15,8 @@ function App() {
     <Navbar></Navbar>
     <h1 className='text-5xl text-purple-600 my-2 text-center'>Hello From Tailwind</h1>
      <PriceList></PriceList>
-      
+      <Dashboard></Dashboard>
+      <PhoneBar></PhoneBar>
     </>
   )
 }
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const Dashboard = () => {
+    const students = [
+        {
+            id: 1,
+            name: 'Student 1'
+            , physics: 85,
+            chemistry: 90,
+            math: 78
+        },
+
+        {
+            id: 2,
+            name: 'Student 2',
+            physics: 75, chemistry: 82,
+            math: 88
+        },
+
+        {
+            id: 3,
+            name: 'Student 3',
+            physics: 92,
+            chemistry: 85,
+            math: 76
+        },
+        {
+            id: 4,
+            name: 'Student 4',
+            physics: 80,
+            chemistry: 88,
+            math: 94
+        },
+        {
+            id: 5,
+            name: 'Student 5',
+            physics: 89,
+            chemistry: 77,
+            math: 85
+        },
+        {
+            id: 6,
+            name: 'Student 6',
+            physics: 78,
+            chemistry: 93,
+            math: 87
+        },
+        {
+            id: 7,
+            name: 'Student 7',
+            physics: 86,
+            chemistry: 79,
+            math: 91
+        },
+        {
+            id: 8,
+            name: 'Student 8',
+            physics: 94,
+            chemistry: 76,
+            math: 80
+        },
+        {
+            id: 9,
+            name: 'Student 9',
+            physics: 83,
+            chemistry: 89,
+            math: 83
+        },
+        {
+            id: 10,
+            name: 'Student 10',
+            physics: 91,
+            chemistry: 81,
+            math: 79
+        },
+        {
+            id: 11,
+            name: 'Student 11',
+            physics: 70,
+            chemistry: 65,
+            math: 72
+        },
+        {
+            id: 12,
+            name: 'Student 12',
+            physics: 88,
+            chemistry: 92,
+            math: 84
+        }
+    ];
+
+
+    return (
+        <div>
+            <LineChart
+                width={1000}
+                height={300}
+                data={students}
+            >
+                <Line dataKey='physics'></Line>
+                <Line stroke="#82ca9d" dataKey='math'></Line>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip></Tooltip>
+            </LineChart>
+        </div>
+    );
+};
+
+export default Dashboard;
